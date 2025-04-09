75814ceae4e89e9f54258d276116b990483b4432
SupraSensum <107372360+SupraSensum@users.noreply.github.com>
2025-04-09 10:59:17 -0300

    finish basic logic

--- rock-paper-scissors/src/index.js
+++ rock-paper-scissors/src/index.js
@@ -1,5 +1,8 @@
 console.log("hello, world!");
 
+let computerScore = 0;
+let humanScore = 0;
+
 const items = [
    "rock",
    "paper",
@@ -13,6 +16,66 @@ function getRandValFromZeroTo(maxValExclusive) {
 function getComputerChoice() {
    randomIndex = getRandValFromZeroTo(items.length);
    return items[randomIndex];
+}
+
+function getHumanChoice() {
+   return prompt("Enter your choice");
+}
+
+function playRound(humanChoice, computerChoice) {
+   humanChoice = humanChoice.toLowerCase();
+
+   let humanWins = false;
+
+   // rock > scissors
+   // paper > rock
+   // scissors > paper
+
+   // switch(humanChoice) {
+   //    case "rock":
+   //       if (computerChoice === "scissors") {
+   //          humanWins = true;
+   //       }
+   //       break;
+   //    case "paper":
+   //       if(computerChoice === "rock") {
+   //          humanWins = true;
+   //       }
+   //       break;
+   //    case "scissors":
+   //       if (computerChoice === "paper") {
+   //          humanWins = true;
+   //       }
+   //       break;
+   //    default:
+   //       humanWins = null;
+   // }
+
+   if (humanChoice === "rock" && computerChoice === "scissors")
+      humanWins = true;
+   else if (humanChoice === "paper" && computerChoice === "rock")
+      humanWins = true;
+   else if (humanChoice === "scissors" && computerChoice === "paper")
+      humanWins = true;
+
+   if (humanWins) {
+      console.log(`You win! ${humanChoice} beats ${computerChoice}.`);
+      humanScore++;
+   } else {
+      console.log(`You lose! ${computerChoice} beats ${humanChoice}.`);
+      computerScore++
+   }
+}
+
+function playGame(numRounds) {
+   for (let i = 0; i < numRounds; i++) {
+      playRound(getComputerChoice(), getComputerChoice());
+   }
+
+   console.log({humanScore, computerScore});
+
+   humanScore = 0;
+   computerScore = 0;
 }
 
 function runRandomnessStats(cycles) {
