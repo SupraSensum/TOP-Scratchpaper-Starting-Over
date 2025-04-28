5cf69f7d41ae15e97e5682fefb116715cfb5aecd
SupraSensum <107372360+SupraSensum@users.noreply.github.com>
2025-04-28 10:16:51 -0300

    test bubbling vs capture

--- dom-manipulation-and-events/src/index.js
+++ dom-manipulation-and-events/src/index.js
@@ -37,12 +37,51 @@ for (let i = 0; i < buttonCopies; i++) {
    buttons.push(btn);
 }
 
+// testing out event bubbling... wow
+const bubbleButtons = [];
+const bubbleButtonCopies = 3;
+for (let i = 0; i < bubbleButtonCopies; i++) {
+   const btn = document.createElement('button');
+   btn.id = `btn-${i}`;
+   btn.textContent = "Click Me!";
+   bubbleButtons.push(btn);
+}
+const bubbleButtonsContainter = document.createElement('div');
+bubbleButtonsContainter.append(...bubbleButtons);
+bubbleButtonsContainter.addEventListener("click", applyButtonStyle);
+
+// create a modal
+const modal = document.createElement('div')
+modal.id = 'modal';
+modal.style.cssText = "outline: 1px solid red";
+const modalButton = document.createElement('button');
+modalButton.id = 'close-button';
+modalButton.textContent = "Close"
+const modalPara = document.createElement('p');
+modalPara.textContent = "Modal content";
+modal.append(modalButton, modalPara);
+
+// JavaScript
+modal.addEventListener("click", function () {
+   console.log("Modal clicked! Closing...");
+   this.style.display = "none"; // Close modal
+});
+
+modalButton.addEventListener("click", function (event) {
+   event.stopPropagation(); // Prevent bubbling up to modal
+   console.log("Close button clicked!");
+   modal.style.display = "none"; // Close modal
+});
+
+
 body.append(
    header,
    redPara,
    subHeader,
    subContainer,
-   ...buttons
+   ...buttons,
+   bubbleButtonsContainter,
+   modal
 );
 
 function applyButtonStyle(e) {
